f05d72b5c0c0f9711a2b7497ae364e82af6407f0
kevinalappat09 <35099148+kevinalappat09@users.noreply.github.com>
2024-02-29 15:01:52 +0530

    Add product filters

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,32 +1,80 @@
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
-const Products = (products) => {
+const Products = ({products}) => {
     const [currentProducts, setCurrentProducts] = useState([]);
     
-    const [mensClothingFilter, setMensClothingFilter] = useState(0);
-    const [womensClothingFilter, setWomensClothingFilter] = useState(0);
-    const [ jewelleryFilter, setJewelleryFilter] = useState(0);
-    const [electronicsFilter, setElectronicsFilter] = useState(0);
+    const [mensClothingFilter, setMensClothingFilter] = useState(false);
+    const [womensClothingFilter, setWomensClothingFilter] = useState(false);
+    const [ jewelleryFilter, setJewelleryFilter] = useState(false);
+    const [electronicsFilter, setElectronicsFilter] = useState(false);
 
     const [sort, setSort] = useState(0);
 
-    const addMensClothingFilter = () => {
-        setFilters(currentState => [...currentState, "mens"]);
+    const switchMensClothingFilter = () => {
+        setMensClothingFilter(!mensClothingFilter);
+    }
+
+    const switchWomensClothingFitler = () => {
+        setWomensClothingFilter(!womensClothingFilter);
+    }
+
+    const switchJewelleryFilter = () => {
+        setJewelleryFilter(!jewelleryFilter);
+    }
+
+    const switchElectronicsFilter = () => {
+        setElectronicsFilter(!electronicsFilter);
     }
 
     useEffect(() => {
-        // set the current products based on the mens clothing, womens clothing and other filters and sorts.
-    })
+        let filteredProducts = [...products]
+        if(mensClothingFilter === true) {
+            const mensClothingItems = filteredProducts.filter(element => element.category === "men's clothing");
+            setCurrentProducts(prevElements => [...prevElements, ...mensClothingItems]);
+        }
+        if(womensClothingFilter === true) {
+            const womensClothingItems = filteredProducts.filter(element => element.category === "women's clothing");
+            setCurrentProducts(prevElements => [...prevElements, ...womensClothingItems]);
+        }
+        if(jewelleryFilter === true) {
+            const jewelleryItems = filteredProducts.filter(element => element.category === "jewelery");
+            setCurrentProducts(prevElements => [...prevElements, ...jewelleryItems]);
+        }
+        if(electronicsFilter === true) {
+            const electronicsItems = filteredProducts.filter(element => element.category === "electronics");
+            setCurrentProducts(prevElements => [...prevElements, ...electronicsItems]);
+        }
+        
+        if(mensClothingFilter === false && womensClothingFilter === false && jewelleryFilter === false && electronicsFilter === false) {
+            setCurrentProducts(prevElements => [...prevElements, ...products]);
+        }
 
-    console.log(products);
+        return () => {
+            setCurrentProducts([]);
+        }
+    }, [mensClothingFilter, womensClothingFilter, jewelleryFilter, electronicsFilter, products]);
+
+    console.log(currentProducts);
     return (
         <div className="products">
-            <button onClick={addMensClothingFilter}>Add mens clothing filter</button>
-            <button>Add womens clothing filter</button>
-            <button>Add jewellery filter</button>
-            <button>Add electronics filter</button>
+            <button onClick={switchMensClothingFilter}>Add mens clothing filter</button>
+            <button onClick={switchWomensClothingFitler}>Add womens clothing filter</button>
+            <button onClick={switchJewelleryFilter}>Add jewellery filter</button>
+            <button onClick={switchElectronicsFilter}>Add electronics filter</button>
+
+            {currentProducts.map(element => (
+                <div key={element.id}>
+                    Name : {element.title}
+                    Category : {element.category}
+                </div>
+            ))}
         </div>
     );
 }
 
 export default Products;
+
+Products.propTypes = {
+    products : PropTypes.array
+};
