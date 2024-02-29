857be731956b7bad99ca294c31b8833c82e124db
kevinalappat09 <35099148+kevinalappat09@users.noreply.github.com>
2024-02-29 18:12:53 +0530

    Fixed issue of pagination

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 
 const Products = ({products}) => {
     const [currentProducts, setCurrentProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productsPerPage] = useState(5);
+    const [maxProducts, setMaxProducts] = useState();
+
     
     const [mensClothingFilter, setMensClothingFilter] = useState(false);
     const [womensClothingFilter, setWomensClothingFilter] = useState(false);
@@ -27,48 +31,107 @@ const Products = ({products}) => {
         setElectronicsFilter(!electronicsFilter);
     }
 
+    const switchSortHighToLow = () => {
+        if(sort === 1) {
+            setSort(0);
+        } else {
+            setSort(1);
+        }
+    }
+
+    const switchSortLowToHigh = () => {
+        if(sort === -1) {
+            setSort(0);
+        } else {
+            setSort(-1);
+        }
+    }
+
+    const prevPage = () => {
+        setCurrentPage(currentPage => currentPage-1);
+    }
+
+    const nextPage = () => {
+        setCurrentPage(currentPage => currentPage+1);
+    }
+
     useEffect(() => {
-        let filteredProducts = [...products]
+
+        const sortElements = (unsortedElements) => {
+            if(sort === 1) {
+                const unsortedProducts = unsortedElements;
+                const sortedProducts = unsortedProducts.sort((a, b) => b.price - a.price);
+                return sortedProducts;
+            } 
+            if(sort === -1) {
+                const unsortedProducts = unsortedElements;
+                const sortedProducts = unsortedProducts.sort((a, b) => a.price - b.price);
+                return sortedProducts;
+            } else {
+                return unsortedElements
+            }
+        }
+
+        const applyPagination = (data) => {
+            const indexOflLastProduct = currentPage * productsPerPage;
+            const indexOfFirstProduct = indexOflLastProduct - productsPerPage;
+            return data.slice(indexOfFirstProduct, indexOflLastProduct);
+        };
+
+        let updatedProducts = [];
+
         if(mensClothingFilter === true) {
-            const mensClothingItems = filteredProducts.filter(element => element.category === "men's clothing");
-            setCurrentProducts(prevElements => [...prevElements, ...mensClothingItems]);
+            updatedProducts = [...updatedProducts, ...(products.filter(element => element.category === "men's clothing"))];
+            
         }
         if(womensClothingFilter === true) {
-            const womensClothingItems = filteredProducts.filter(element => element.category === "women's clothing");
-            setCurrentProducts(prevElements => [...prevElements, ...womensClothingItems]);
+            updatedProducts = [...updatedProducts, ...(products.filter(element => element.category === "women's clothing"))] ;
         }
         if(jewelleryFilter === true) {
-            const jewelleryItems = filteredProducts.filter(element => element.category === "jewelery");
-            setCurrentProducts(prevElements => [...prevElements, ...jewelleryItems]);
+            updatedProducts = [...updatedProducts, ...(products.filter(element => element.category === "jewelery"))];
         }
         if(electronicsFilter === true) {
-            const electronicsItems = filteredProducts.filter(element => element.category === "electronics");
-            setCurrentProducts(prevElements => [...prevElements, ...electronicsItems]);
+            updatedProducts = [...updatedProducts, ...(products.filter(element => element.category === "electronics"))];
         }
         
         if(mensClothingFilter === false && womensClothingFilter === false && jewelleryFilter === false && electronicsFilter === false) {
-            setCurrentProducts(prevElements => [...prevElements, ...products]);
+            const sortedElements = sortElements(products);
+            setMaxProducts(products.length);
+            const paginatedData = applyPagination(sortedElements);
+            setCurrentProducts(prevElements => [...prevElements, ...paginatedData]);
+        } else {
+            const sortedElements = sortElements(updatedProducts);
+            setMaxProducts(updatedProducts.length);
+            const paginatedData = applyPagination(sortedElements);
+            setCurrentProducts(prevElements => [...prevElements, ...paginatedData]);
         }
 
+        
         return () => {
             setCurrentProducts([]);
         }
-    }, [mensClothingFilter, womensClothingFilter, jewelleryFilter, electronicsFilter, products]);
+    }, [mensClothingFilter, womensClothingFilter, jewelleryFilter, electronicsFilter, sort, products, currentPage, productsPerPage]);
 
-    console.log(currentProducts);
+
     return (
         <div className="products">
             <button onClick={switchMensClothingFilter}>Add mens clothing filter</button>
             <button onClick={switchWomensClothingFitler}>Add womens clothing filter</button>
             <button onClick={switchJewelleryFilter}>Add jewellery filter</button>
             <button onClick={switchElectronicsFilter}>Add electronics filter</button>
+            <button onClick={switchSortHighToLow}>Add sort high to low</button>
+            <button onClick={switchSortLowToHigh}>Add sort low to high</button>
 
             {currentProducts.map(element => (
-                <div key={element.id}>
-                    Name : {element.title}
-                    Category : {element.category}
+                <div key={element.id}> 
+                    Name : {element.title} ||
+                    Category : {element.category} ||
+                    Price : {element.price}
                 </div>
             ))}
+
+            <button onClick={prevPage} disabled={currentPage === 1}>Previous Page</button>
+            <button onClick={nextPage} disabled={currentPage * productsPerPage > maxProducts-1}>Next Page</button>
         </div>
     );
 }
